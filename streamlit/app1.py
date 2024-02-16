bccfd76c45a6800bdf233564c6c4a8d693bdefa4
belseris <ratthapak.si.66@ubu.ac.th>
2024-02-16 15:44:54 +0700

    add app

---
+++ streamlit/app1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+
+#streamlit run streamlit/app1.py  
+
+
---
+++ streamlit/app2.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.hrader('MY OOP')
+st.title('OOP in ML')
+st.sideber.text('Sidebre')
+st.sideber.button('go>')
+st.sideber.button('went>')
+st.sideber.button('gone>')
+st.title('OOP in ML')
+st.image('/500/600')
+st.text_input("What's your name")
+
+
