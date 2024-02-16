b815381dcd0b82701840f151c317fd17455ebaa2
belseris <ratthapak.si.66@ubu.ac.th>
2024-02-16 16:02:20 +0700

    add jpy

--- streamlit/app2.py
+++ streamlit/app2.py
@@ -6,6 +6,6 @@ st.sideber.button('go>')
 st.sideber.button('went>')
 st.sideber.button('gone>')
 st.title('OOP in ML')
-st.image('/500/600')
+st.image('bl1.jpy/500/600')
 st.text_input("What's your name")
 
