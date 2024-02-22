0927fa46a0619aef2a18c6b5e6fce8ed84c8b749
belseris <ratthapak.si.66@ubu.ac.th>
2024-02-22 18:05:19 +0700

    app งานวิชิต

--- streamlit/app2.py
+++ streamlit/app2.py
@@ -1,24 +1,10 @@
-import streamlit as st
-st.header('MY OOP')
-st.subheader('เว็บไซต์ส่วนตัวของฉัน')
-st.write('เว็บไซต์นี้เเลกหยาดเหงื่อเเละความอดทน')
-banner = st.image('https://picsum.photos/800/250')
-text = st.text_input('prompt: ')
-if text:
-    # st.write(f'กำลังสร้างภาพ...."{text}"')
-    st.image('https://picsum.photos/800/250')
-    b = st.button('จะไปต่อหรือ...')
+import streamlit
+
+h = streamlit.header('My Web Site on Diffusion')
+s = streamlit.subheader('เว็บไซต์ส่วนตัวของฉานน')
+p = streamlit.write('เว็บไซต์นี้แลกมาด้วยหยาดเหงื่อและความอดทน')
+banner = streamlit.image('https://picsum.photos/800/250')
+b = streamlit.button('click me')
 
 
-
-
-
-# st.title('OOP in ML')
-# st.sideber.text('Sidebre')
-# st.sideber.button('go>')
-# st.sideber.button('went>')
-# st.sideber.button('gone>')
-# st.title('OOP in ML')
-# st.image('bl1.jpy/500/600')
-# st.text_input("What's your name")
 
